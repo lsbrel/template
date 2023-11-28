2c7713f4ab6012315b3391cc1965cdbe8fc71356
Lucas <lucas.81.gabriel@gmail.com>
2023-11-27 23:01:18 -0300

    🦄 refactor: responsividade horarios

--- src/components/home/Horarios.vue
+++ src/components/home/Horarios.vue
@@ -44,29 +44,31 @@ export default {
 }
 </script>
 <template>
-  <div :class="classePage">
-    <div v-for="dia in dias" class="card mb-3 p-3">
-      <img
-        class="card-img-top w-25"
-        src="https://static.vecteezy.com/system/resources/previews/000/550/428/original/soccer-ball-vector-icon.jpg"
-        alt="Card image cap"
-      />
-      <div class="card-body">
-        <h5 class="card-title text-capitalize">{{ dia }}</h5>
-        <p class="card-text"><small class="text-muted">01/07/2024</small></p>
-        <hr />
-        <div v-for="(horario, index) in 4" class="card-text flex flex-column w-100">
-          <div
-            v-if="horario.id == 0"
-            class="item-horario w-100 bg-secondary rounded text-white mt-2"
-          >
-            {{ horarios[0].hora + index }}:00 - {{ horarios[0].nome }}
+  <div class="container-fluid">
+    <div :class="classePage">
+      <div v-for="dia in dias" class="card col-10 col-lg-4 mb-3 p-3">
+        <img
+          class="card-img-top w-25"
+          src="https://static.vecteezy.com/system/resources/previews/000/550/428/original/soccer-ball-vector-icon.jpg"
+          alt="Card image cap"
+        />
+        <div class="card-body">
+          <h5 class="card-title text-capitalize">{{ dia }}</h5>
+          <p class="card-text"><small class="text-muted">01/07/2024</small></p>
+          <hr />
+          <div v-for="(horario, index) in 4" class="card-text flex flex-column w-100">
+            <div
+              v-if="horario.id == 0"
+              class="item-horario w-100 bg-secondary rounded text-white mt-2"
+            >
+              {{ horarios[0].hora + index }}:00 - {{ horarios[0].nome }}
+            </div>
+            <div v-else class="item-horario w-100 bg-success rounded-1 text-white mt-2">
+              {{ horarios[0].hora + index }}:00 - {{ horarios[0].nome }}
+            </div>
           </div>
-          <div v-else class="item-horario w-100 bg-success rounded-1 text-white mt-2">
-            {{ horarios[0].hora + index }}:00 - {{ horarios[0].nome }}
-          </div>
+          <p class="card-text"><small class="text-muted">*** Sujeito a alterações</small></p>
         </div>
-        <p class="card-text"><small class="text-muted">*** Sujeito a alterações</small></p>
       </div>
     </div>
   </div>
